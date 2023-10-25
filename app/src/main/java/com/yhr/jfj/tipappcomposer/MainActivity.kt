b4d11ff491e70fe5dbeb457267bbbcb44090dff5
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-25 17:02:46 +0600

    Basic Things

--- app/src/main/java/com/yhr/jfj/tipappcomposer/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/tipappcomposer/MainActivity.kt
@@ -16,31 +16,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipAppComposerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Text(text = "YHRJFJ")
             }
         }
     }
 }
 
+// Main Function
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    TipAppComposerTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            content()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TipAppComposerTheme {
-        Greeting("Android")
+        MyApp {
+            Text(text = "YHRJFJ")
+        }
     }
 }
