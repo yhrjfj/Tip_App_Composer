bccb436a27691ed49ee62f21fb13b9d8b6681c77
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-25 18:00:53 +0600

    Done TopHeader part

--- app/src/main/java/com/yhr/jfj/tipappcomposer/MainActivity.kt
+++ app/src/main/java/com/yhr/jfj/tipappcomposer/MainActivity.kt
@@ -3,10 +3,13 @@ package com.yhr.jfj.tipappcomposer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,10 +17,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yhr.jfj.tipappcomposer.ui.theme.TipAppComposerTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "YHRJFJ")
+                TopHeader()
             }
         }
     }
@@ -47,16 +55,32 @@ fun MyApp(content: @Composable () -> Unit) {
 // TopHeader Functiion where user can see how much money the need to pay
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
-            .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
-//            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+//            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
     ) {
-        Column {
-
+        Column(
+            modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total per person",
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.ExtraBold
+            )
         }
     }
 }
